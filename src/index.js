1236ac2dd152ef22f9d598ff7d3a73afc20326a7
Zach M <zachdtr@aol.com>
2018-10-03 15:50:29 -0600

    Added HAsh Router and package elements

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { HashRouter } from 'react-router-dom'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'))
